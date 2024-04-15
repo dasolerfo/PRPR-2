337c3ef780bf7cc7d967dd61b32d4892ad7a2011
Roger Metaute Carrillo <roger.metaute@students.salle.url.edu>
2024-04-15 11:37:09 +0200

    Actulització i iniciar l'estructura dels fragments

--- app/src/main/java/com/example/pokedexls/MainActivity.java
+++ app/src/main/java/com/example/pokedexls/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.pokedexls;
 
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentManager);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fragmentManager.beginTransaction().add(R.id.fragmentManager, fragment).commit();
+        }
+
     }
+
+
 }
