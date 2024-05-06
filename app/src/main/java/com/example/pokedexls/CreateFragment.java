1c4b834649ccd43acda254b10e0e0a8ea02d040e
Roger Metaute Carrillo <roger.metaute@students.salle.url.edu>
2024-05-06 11:00:54 +0200

    Actualització

--- app/src/main/java/com/example/pokedexls/CreateFragment.java
+++ app/src/main/java/com/example/pokedexls/CreateFragment.java
@@ -5,6 +5,6 @@ import androidx.fragment.app.Fragment;
 public class CreateFragment extends MainActivity{
     @Override
     protected Fragment createFragment() {
-        return null;
+        return new ShopFragment();
     }
 }
