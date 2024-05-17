377d3083e1114bd943b6919f00984d45af79d02b
Roger Metaute Carrillo <roger.metaute@students.salle.url.edu>
2024-05-18 00:48:30 +0200

    Act tenda + init persistance

---
+++ app/src/main/java/com/example/pokedexls/Entity/Trainer.java
@@ -0,0 +1,13 @@
+package com.example.pokedexls.Entity;
+
+import java.util.ArrayList;
+
+public class Trainer {
+    private String name;
+    private int numPokeballs;
+    private int numSuperballs;
+    private int numUltraballs;
+    private int numMasterballs;
+    private int money;
+    private ArrayList<Pokemon> pokemons;
+}
